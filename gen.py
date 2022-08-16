fcd7ab64e7fab28952c9879495e81466813a5157
Wojciech Sronek <w.sronek@f5.com>
2022-08-16 12:42:46 +0200

    New requests

--- gen.py
+++ gen.py
@@ -1,6 +1,6 @@
 from locust import HttpUser, task
 
-class HelloWorldUser(HttpUser):
+class APISecurityValidation(HttpUser):
 
     @task
     def hello_world(self):
@@ -10,3 +10,9 @@ class HelloWorldUser(HttpUser):
     @task
     def index(self):
         self.client.get("/")
+
+    def post_username_password(self):
+        self.client.post("/login", {"username":"testuser", "password":"secret"})
+
+    def post_username_password_json(self):
+        self.client.post("/login", json={"username":"testuser", "password":"secret"})
