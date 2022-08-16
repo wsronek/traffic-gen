e06637d8d4c01ed64eb5549ed8e8fc58b57f38d1
Wojciech Sronek <w.sronek@f5.com>
2022-08-16 13:30:02 +0200

    Traffic generator draft1

--- gen.py
+++ gen.py
@@ -1,6 +1,7 @@
 from locust import HttpUser, task
 
 class APISecurityValidation(HttpUser):
+    host = "https://sol-eng-lb2.perf.f5xc.app"
 
     @task
     def hello_world(self):
@@ -11,8 +12,16 @@ class APISecurityValidation(HttpUser):
     def index(self):
         self.client.get("/")
 
+    @task
     def post_username_password(self):
         self.client.post("/login", {"username":"testuser", "password":"secret"})
 
+    @task
     def post_username_password_json(self):
         self.client.post("/login", json={"username":"testuser", "password":"secret"})
+
+    @task
+    def view_items(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            time.sleep(1)
