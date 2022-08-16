7cce2f0f74681d58cc59f050a59ee4e786463318
Wojciech Sronek <w.sronek@f5.com>
2022-08-16 17:19:22 +0200

    Additional Traffic Generator Spawned in AWS

--- gen.py
+++ gen.py
@@ -1,12 +1,39 @@
-from locust import HttpUser, task
+import time
+import random
+from locust import HttpUser, between, task
+
+products = [
+    '0PUK6V6EV0',
+    '1YMWWN1N4O',
+    '2ZYFJ3GM2N',
+    '66VCHSJNUP',
+    '6E92ZMYYFZ',
+    '9SIQT8TOJO',
+    'L9ECAV7KIM',
+    'LS4PSXUNUM',
+    'OLJCESPC7Z']
 
 class APISecurityValidation(HttpUser):
     host = "https://sol-eng-lb2.perf.f5xc.app"
+    
+    wait_time = between(3, 10)
+
+    def on_start(self):
+        self.client.headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
 
     @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+    def methods_head(self):
+        self.client.head("/")
+
+    @task
+    def create_different_paths(self):
+        for path_id in range(1000):
+            self.client.get(f"/dynpath{path_id}")
+            time.sleep(1)
+
+    @task
+    def get_review(self):
+        self.client.get("/review")
 
     @task
     def index(self):
@@ -14,7 +41,8 @@ class APISecurityValidation(HttpUser):
 
     @task
     def post_username_password(self):
-        self.client.post("/login", {"username":"testuser", "password":"secret"})
+        for path_id in range(1000):
+            self.client.post(f"/login{path_id}", {"username":"testuser", "password":"secret"})
 
     @task
     def post_username_password_json(self):
@@ -25,3 +53,41 @@ class APISecurityValidation(HttpUser):
         for item_id in range(10):
             self.client.get(f"/item?id={item_id}", name="/item")
             time.sleep(1)
+
+    @task
+    def setCurrency(self):
+        currencies = ['EUR', 'USD', 'JPY', 'CAD']
+        self.client.post("/setCurrency",
+            {'currency_code': random.choice(currencies)})
+
+    @task
+    def browseProduct(self):
+        self.client.get("/product/" + random.choice(products))
+
+    @task
+    def viewCart(self):
+        self.client.get("/cart")
+
+    @task
+    def addToCart(self):
+        product = random.choice(products)
+        self.client.get("/product/" + product)
+        self.client.post("/cart", {
+            'product_id': product,
+            'quantity': random.choice([1,2,3,4,5,10])})
+
+    @task
+    def checkout(self):
+        self.addToCart()
+        self.client.post("/cart/checkout", {
+            'email': 'someone@example.com',
+            'street_address': '1600 Amphitheatre Parkway',
+            'zip_code': '94043',
+            'city': 'Mountain View',
+            'state': 'CA',
+            'country': 'United States',
+            'credit_card_number': '4432-8015-6152-0454',
+            'credit_card_expiration_month': '1',
+            'credit_card_expiration_year': '2039',
+            'credit_card_cvv': '672',
+        })
