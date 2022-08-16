e9d69c03c13d293cbe939370e2e6466e064c719b
Wojciech Sronek <w.sronek@f5.com>
2022-08-16 12:32:48 +0200

    Add gen.py

---
+++ gen.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task
+
+class HelloWorldUser(HttpUser):
+
+    @task
+    def hello_world(self):
+        self.client.get("/hello")
+        self.client.get("/world")
+
+    @task
+    def index(self):
+        self.client.get("/")
